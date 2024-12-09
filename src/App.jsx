d43194b4af7bac121c7098192811032589277c4c
Siddhartha Konduru <kondurus@usf.edu>
2024-12-08 19:45:26 -0500

    Fetch chart data from JSON file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 // Task 4 
 // Fetch Data for Charts
+import React, { useState, useEffect } from 'react';
+import BarChart from './components/BarChart';
+import LineChart from './components/LineChart';
+import ScatterChart from './components/ScatterChart';
+import BubbleChart from './components/BubbleChart';
 
+const App = () => {
 
+  const [chartData, setChartData] = useState(null);
+
+  useEffect(() => {
+
+    fetch('/financial_data.json') 
+      .then((response) => response.json())
+      .then((data) => setChartData(data)); 
+
+  }, []);
+
+}
 // Task 5 
 // Render Charts in the App
